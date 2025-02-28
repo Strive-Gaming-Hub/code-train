c8e7725807828eebd6f389a86b52df8b5752cb06
Sparsh47 <sparshshandilya123@gmail.com>
2025-02-28 17:11:43 +0530

    re added tabs in pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -2,8 +2,10 @@ import './pricingcards.css';
 import PricingCard from './PricingCard';
 import { motion, AnimatePresence } from 'framer-motion';
 import { useNavigate } from 'react-router-dom';
+import {useState} from "react";
 
 const Pricing = () => {
+    const [tab, setTab] = useState('quarter');
     const navigate = useNavigate();
 
     const handleCTAClick = () => {
@@ -20,35 +22,75 @@ const Pricing = () => {
         visible: { y: 0, opacity: 1, filter: 'blur(0px)', transition: { duration: 0.8, ease: 'easeOut' } },
     };
 
-    const pricingData = [
-        {
-            price: 4999,
-            time: "month",
-            plan: "Basic Plan",
-            icon: "🚀",
-            cta: "Contact Us to Buy",
-            features: [
-                "Customizable templates",
-                "Full ecommerce portal",
-                "Secure payments & shipping",
-                "Meta Pixel & Google Shopping integration",
-                "GST compliant site and invoicing",
-            ],
-        },
-        {
-            price: "Price on Request",
-            plan: "Custom Website Plan",
-            icon: "⚡",
-            highlight: "Popular",
-            cta: "Contact Us to Buy",
-            features: [
-                "All features in Basic Plan",
-                "Fully custom templates",
-                "Tailored features & integrations",
-                "Expert guidance & support",
-            ],
-        },
-    ];
+    const tabVariants = {
+        hidden: { opacity: 0, y: 20 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.5, ease: 'easeOut' } },
+    };
+
+    const pricingData = {
+        quarter: [
+            {
+                price: 4999,
+                time: "month",
+                plan: "Basic Plan",
+                paidTimes: "quarterly",
+                icon: "🚀",
+                cta: "Contact Us to Buy",
+                features: [
+                    "Customizable templates",
+                    "Full ecommerce portal",
+                    "Secure payments & shipping",
+                    "Meta Pixel & Google Shopping integration",
+                    "GST compliant site and invoicing",
+                ],
+            },
+            {
+                price: "Price on Request",
+                plan: "Custom Website Plan",
+                paidTimes: "quarterly",
+                icon: "⚡",
+                highlight: "Popular",
+                cta: "Contact Us to Buy",
+                features: [
+                    "All features in Basic Plan",
+                    "Fully custom templates",
+                    "Tailored features & integrations",
+                    "Expert guidance & support",
+                ],
+            },
+        ],
+        annual: [
+            {
+                price: 3999,
+                time: "month",
+                plan: "Basic Plan",
+                paidTimes: "annually",
+                icon: "🚀",
+                cta: "Contact Us to Buy",
+                features: [
+                    "Customizable templates",
+                    "Full ecommerce portal",
+                    "Secure payments & shipping",
+                    "Meta Pixel & Google Shopping integration",
+                    "GST compliant site and invoicing",
+                ],
+            },
+            {
+                price: "Price on Request",
+                plan: "Custom Website Plan",
+                paidTimes: "annually",
+                icon: "⚡",
+                highlight: "Popular",
+                cta: "Contact Us to Buy",
+                features: [
+                    "All features in Basic Plan",
+                    "Fully custom templates",
+                    "Tailored features & integrations",
+                    "Expert guidance & support",
+                ],
+            },
+        ]
+    };
 
     return (
         <div id='pricing' className='md:py-20 py-14 md:px-20'>
@@ -71,8 +113,46 @@ const Pricing = () => {
                 Choose a plan that gives you the flexibility and power to build your dream store.
             </motion.p>
 
+            <div className="w-full flex items-center justify-center mb-10">
+                <motion.div
+                    className="relative flex items-center justify-center gap-2 rounded-lg p-1 border border-blue-800/50"
+                    initial="hidden"
+                    animate="visible"
+                    variants={tabVariants}
+                >
+                    <motion.div
+                        className="absolute w-1/2 h-full bg-gradient-to-br from-[#6784ff] to-[#4462ff] rounded-md"
+                        layout
+                        transition={{ type: "spring", stiffness: 250, damping: 20 }}
+                        style={{
+                            left: tab === "quarter" ? "0%" : "50%",
+                            top: 0,
+                        }}
+                    />
+
+                    <button
+                        onClick={() => setTab("quarter")}
+                        className={`px-10 py-2 rounded-md uppercase transition-all duration-300 relative z-10 ${
+                            tab === "quarter" ? "text-white" : "hover:bg-indigo-950/40"
+                        }`}
+                    >
+                        Quarter
+                    </button>
+
+                    <button
+                        onClick={() => setTab("annual")}
+                        className={`px-10 py-2 rounded-md uppercase transition-all duration-300 relative z-10 ${
+                            tab === "annual" ? "text-white" : "hover:bg-indigo-950/40"
+                        }`}
+                    >
+                        Annual
+                    </button>
+                </motion.div>
+            </div>
+
+
             <motion.div className="pricing-container">
-                {pricingData.map((pricing, index) => (
+                {pricingData[tab].map((pricing, index) => (
                     <motion.div
                         key={index}
                         initial={{ opacity: 0, y: 20 }}
--- src/components/PricingCard.jsx
+++ src/components/PricingCard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 
-const PricingCard = ({ plan, features, price, comparison, cta, icon, highlight, time }) => {
+const PricingCard = ({ plan, features, price, comparison, cta, icon, highlight, time, paidTimes }) => {
     const cardRef = useRef(null);
 
     useEffect(() => {
@@ -65,7 +65,7 @@ const PricingCard = ({ plan, features, price, comparison, cta, icon, highlight, 
             {price && <div className="price">
                 {price}{time && <span>/{time}</span>}
             </div>}
-            <p className="payment-frequency">Paid quarterly</p>
+            <p className="payment-frequency">Paid {paidTimes}</p>
             <ul className="features">
                 {
                     features.map((feat, i) => (
